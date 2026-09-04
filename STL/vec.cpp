38f2fe496ae5e4df984a05f144dffdd52ce97600
Angkur12 <angkurpaul95@gmail.com>
2026-09-04 08:35:51 +0600

    stl revision

---
+++ STL/vec.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()  {
+  vector<int> vec;
+  vec.push_back(1);
+  vec.push_back(2);
+  vec.push_back(3);
+
+  for(int val: vec) {
+    cout<<val<<" ";
+  }
+  cout<<endl; 
+
+  cout<<vec.capacity()<<endl;
+}
