e1badf4143cdbb46d2fd9d8172cfb345fb45e6f4
Angkur12 <angkurpaul95@gmail.com>
2026-09-05 16:46:21 +0600

    vector of pair

--- STL/pair.cpp
+++ STL/pair.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 int main()  {
-  pair<int, pair<int,int>> p = {1, {2,3}};
-  cout<<p.first<<endl;
-  cout<<p.second.first<<endl;
-  cout<<p.second.second<<endl;
+  // pair<int, pair<int,int>> p = {1, {2,3}};
+  // cout<<p.first<<endl;
+  // cout<<p.second.first<<endl;
+  // cout<<p.second.second<<endl;
+
+  vector<pair<int,int>> vec = {{1,2}, {2,3}, {3,4}};
+  vec.push_back({4,5});
+  vec.emplace_back(4,5);
+  for(auto p: vec) {
+    cout<<p.first<<" "<<p.second<<endl;
+  }
 
   return 0;
 }
