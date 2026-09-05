87d95a551f3ef7d782cd754c4c77857e47b858b6
Angkur12 <angkurpaul95@gmail.com>
2026-09-05 15:48:33 +0600

    initializing list

--- STL/list.cpp
+++ STL/list.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main()  {
-  list<int> l;
-  l.push_back(1);
-  l.push_back(2);
-  l.push_front(3);
-  l.push_front(5);
+  list<int> l = {1,2,3,4,5};
 
   for(int val : l) {
     cout<<val<<" ";
